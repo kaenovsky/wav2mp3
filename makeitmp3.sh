d086fd567dbacb778725fa3d4998929a56f608e3
kaenovsky <martinkaen@gmail.com>
2021-10-08 20:52:04 -0300

    improve name var

--- makeitmp3.sh
+++ makeitmp3.sh
@@ -3,7 +3,7 @@
 # convert all .wav files from current directory to .mp3
 
 for i in *.wav;
-  do name=`echo "$i" | cut -d'.' -f1`
+  do name="${i%.*}"
   echo "$name"
   ffmpeg -i "$i" -vn -ar 44100 -ac 2 -b:a 192k "${name}.mp3"
 done
